be3888fac66e8e17abeb65d455c4dbf95a431693
Neel Shah <neelshahx@gmail.com>
2026-01-06 23:52:00 +0100

    tests

--- project4/src/lib.rs
+++ project4/src/lib.rs
@@ -1,5 +1,6 @@
 use std::vec::Vec;
 
+#[derive(Clone)]
 pub struct LogEntry {
     term_index: usize,
     command: String,
@@ -26,7 +27,8 @@ impl RaftLog {
     }
 
     fn add_new_command(&mut self, leader_term: usize, command: String) {
-        self.log.push(LogEntry::new(leader_term, command));
+        self.log
+            .push(LogEntry::new(leader_term, command));
     }
 
     fn append_entries(
@@ -38,30 +40,30 @@ impl RaftLog {
         if prev_index >= self.log.len() {
             return false;
         }
-        if self.log[prev_index].term_index != prev_term {
+        if self.log.len() > 1 && self.log[prev_index].term_index != prev_term {
             return false;
         }
-        let slice = &self.log[prev_index + 1..];
+        let slice = &self.log[prev_index+1..];
         if slice.is_empty() {
             for entry in entries {
                 self.log.push(entry)
             }
             return true;
         }
+        let mut n: usize = 0;
         for (item1, item2) in slice.iter().zip(entries.iter()) {
-            if item1.term_index != item2.term_index {
-                return false;
+            if item1.term_index == item2.term_index {
+                n += 1;
+            } else {
+                self.log.truncate(prev_index + n + 1);
+                break;
             }
         }
-        for item in entries.into_iter().skip(slice.len()) {
-            self.log.push(item);
+        for item in &entries[n..] {
+            self.log.push(item.clone());
         }
         true
     }
-}
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
 }
 
 #[cfg(test)]
@@ -69,8 +71,54 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_initialise() {
+        let log = RaftLog::new();
+        assert_eq!(log.log.len(), 1);
+    }
+
+    #[test]
+    fn test_add_command() {
+        let mut log = RaftLog::new();
+        log.add_new_command(1, "set name alice".to_string());
+        assert_eq!(log.log[1].term_index, 1);
+        assert_eq!(log.log[1].command, "set name alice");
+    }
+
+    #[test]
+    fn test_append_entries() {
+        let mut log = RaftLog::new();
+        log.add_new_command(1, "set name alice".to_string());
+
+        let entries: Vec<LogEntry> = vec![
+            LogEntry::new(2, "delete name".to_string()),
+            LogEntry::new(2, "set name bob".to_string()),
+        ];
+
+        log.append_entries(1, 1, entries);
+        assert_eq!(log.log.len(), 4);
+        assert_eq!(log.log[1].command, "set name alice");
+        assert_eq!(log.log[2].command, "delete name");
+        assert_eq!(log.log[3].command, "set name bob");
+    }
+
+    #[test]
+    fn test_ae_prev_index_oob() {
+        let mut log = RaftLog::new();
+        log.add_new_command(1, "set name alice".to_string());
+        let entries = vec![LogEntry::new(2, "delete name".to_string())];
+        assert_eq!(log.append_entries(2, 1, entries), false);
+        assert_eq!(log.log.len(), 2);
+        assert_eq!(log.log[1].command, "set name alice");
+    }
+
+
+    #[test]
+    fn test_ae_prev_term_mismatch() {
+        let mut log = RaftLog::new();
+        log.add_new_command(1, "set name alice".to_string());
+        let entries = vec![LogEntry::new(2, "delete name".to_string())];
+        assert_eq!(log.append_entries(1, 2, entries), false);
+        assert_eq!(log.log.len(), 2);
+        assert_eq!(log.log[1].command, "set name alice");
     }
 }
