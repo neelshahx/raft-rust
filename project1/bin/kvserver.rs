adff19912f9082acd8deaf34b09971f3491ac4a2
Neel Shah <neelshahx@gmail.com>
2026-01-06 15:55:26 +0100

    project1 added tests

--- project1/bin/kvserver.rs
+++ project1/bin/kvserver.rs
@@ -78,3 +78,145 @@ fn main() -> Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_existing_key() {
+        let mut store = KVStore::new();
+        store.set("foo", "bar");
+        let result = update_store("get foo".to_string(), &mut store).unwrap();
+        assert_eq!(result, "bar");
+    }
+
+    #[test]
+    fn test_get_nonexistent_key() {
+        let mut store = KVStore::new();
+        let result = update_store("get missing".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Not found");
+    }
+
+    #[test]
+    fn test_set_new_key() {
+        let mut store = KVStore::new();
+        let result = update_store("set mykey myvalue".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Set");
+        assert_eq!(store.get("mykey"), Some("myvalue".to_string()));
+    }
+
+    #[test]
+    fn test_set_existing_key() {
+        let mut store = KVStore::new();
+        store.set("key", "old");
+        let result = update_store("set key new".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Old value: old");
+        assert_eq!(store.get("key"), Some("new".to_string()));
+    }
+
+    #[test]
+    fn test_delete_existing_key() {
+        let mut store = KVStore::new();
+        store.set("foo", "bar");
+        let result = update_store("delete foo".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Deleted value: bar");
+        assert_eq!(store.get("foo"), None);
+    }
+
+    #[test]
+    fn test_delete_nonexistent_key() {
+        let mut store = KVStore::new();
+        let result = update_store("delete missing".to_string(), &mut store).unwrap();
+        assert_eq!(result, "No such key");
+    }
+
+    #[test]
+    fn test_incr_existing_integer() {
+        let mut store = KVStore::new();
+        store.set("counter", "5");
+        let result = update_store("incr counter".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Incr 6");
+        assert_eq!(store.get("counter"), Some("6".to_string()));
+    }
+
+    #[test]
+    fn test_incr_negative_integer() {
+        let mut store = KVStore::new();
+        store.set("counter", "-10");
+        let result = update_store("incr counter".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Incr -9");
+        assert_eq!(store.get("counter"), Some("-9".to_string()));
+    }
+
+    #[test]
+    fn test_incr_zero() {
+        let mut store = KVStore::new();
+        store.set("counter", "0");
+        let result = update_store("incr counter".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Incr 1");
+        assert_eq!(store.get("counter"), Some("1".to_string()));
+    }
+
+    #[test]
+    fn test_incr_noninteger_value() {
+        let mut store = KVStore::new();
+        store.set("key", "notanumber");
+        let result = update_store("incr key".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Invalid: can only incr integral values");
+    }
+
+    #[test]
+    fn test_incr_nonexistent_key() {
+        let mut store = KVStore::new();
+        let result = update_store("incr missing".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Invalid: no such key");
+    }
+
+    #[test]
+    fn test_invalid_command() {
+        let mut store = KVStore::new();
+        let result = update_store("invalid".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Invalid command");
+    }
+
+    #[test]
+    fn test_invalid_get_no_key() {
+        let mut store = KVStore::new();
+        let result = update_store("get".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Invalid command");
+    }
+
+    #[test]
+    fn test_invalid_set_missing_value() {
+        let mut store = KVStore::new();
+        let result = update_store("set key".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Invalid command");
+    }
+
+    #[test]
+    fn test_invalid_delete_no_key() {
+        let mut store = KVStore::new();
+        let result = update_store("delete".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Invalid command");
+    }
+
+    #[test]
+    fn test_extra_whitespace() {
+        let mut store = KVStore::new();
+        let result = update_store("  set   key   value  ".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Set");
+        assert_eq!(store.get("key"), Some("value".to_string()));
+    }
+
+    #[test]
+    fn test_multiple_incr() {
+        let mut store = KVStore::new();
+        store.set("count", "0");
+        update_store("incr count".to_string(), &mut store).unwrap();
+        update_store("incr count".to_string(), &mut store).unwrap();
+        let result = update_store("incr count".to_string(), &mut store).unwrap();
+        assert_eq!(result, "Incr 3");
+        assert_eq!(store.get("count"), Some("3".to_string()));
+    }
+}
--- project1/src/lib.rs
+++ project1/src/lib.rs
@@ -20,3 +20,79 @@ impl KVStore {
         self.data.remove(key)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_store_is_empty() {
+        let store = KVStore::new();
+        assert_eq!(store.get("any_key"), None);
+    }
+
+    #[test]
+    fn test_set_and_get() {
+        let mut store = KVStore::new();
+        let result = store.set("key1", "value1");
+        assert_eq!(result, None);
+        assert_eq!(store.get("key1"), Some("value1".to_string()));
+    }
+
+    #[test]
+    fn test_set_returns_old_value() {
+        let mut store = KVStore::new();
+        store.set("key1", "value1");
+        let old_value = store.set("key1", "value2");
+        assert_eq!(old_value, Some("value1".to_string()));
+        assert_eq!(store.get("key1"), Some("value2".to_string()));
+    }
+
+    #[test]
+    fn test_get_nonexistent_key() {
+        let store = KVStore::new();
+        assert_eq!(store.get("nonexistent"), None);
+    }
+
+    #[test]
+    fn test_delete_existing_key() {
+        let mut store = KVStore::new();
+        store.set("key1", "value1");
+        let deleted = store.delete("key1");
+        assert_eq!(deleted, Some("value1".to_string()));
+        assert_eq!(store.get("key1"), None);
+    }
+
+    #[test]
+    fn test_delete_nonexistent_key() {
+        let mut store = KVStore::new();
+        let result = store.delete("nonexistent");
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_multiple_keys() {
+        let mut store = KVStore::new();
+        store.set("key1", "value1");
+        store.set("key2", "value2");
+        store.set("key3", "value3");
+
+        assert_eq!(store.get("key1"), Some("value1".to_string()));
+        assert_eq!(store.get("key2"), Some("value2".to_string()));
+        assert_eq!(store.get("key3"), Some("value3".to_string()));
+
+        store.delete("key2");
+        assert_eq!(store.get("key2"), None);
+        assert_eq!(store.get("key1"), Some("value1".to_string()));
+        assert_eq!(store.get("key3"), Some("value3".to_string()));
+    }
+
+    #[test]
+    fn test_overwrite_multiple_times() {
+        let mut store = KVStore::new();
+        assert_eq!(store.set("key", "v1"), None);
+        assert_eq!(store.set("key", "v2"), Some("v1".to_string()));
+        assert_eq!(store.set("key", "v3"), Some("v2".to_string()));
+        assert_eq!(store.get("key"), Some("v3".to_string()));
+    }
+}
