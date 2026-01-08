20edd1c238a8a2cef4a5964a8068e9cc9cfdb3f1
Neel Shah <neelshahx@gmail.com>
2026-01-08 17:32:49 +0100

    merge
    # Conflicts:
    #	project5/src/lib.rs
    #	raft/bin/client.rs
    #	raft/src/clienthandler.rs
    #	raft/src/raftconsole.rs
    #	raft/src/shared.rs

--- project5/src/lib.rs
+++ project5/src/lib.rs
@@ -1,2 +1,7 @@
-pub mod raftconsensus;
-pub mod raftlog;
+pub mod clienthandler; // read from client, write to event queue TODO: goes in raftapp
+pub mod raftconsensus; // TODO: adds client command to log, updates followers, updates leader
+pub mod raftconsole; // read from console, write to event queue
+pub mod raftlog; // handles adds/appends to log
+pub mod raftnet; // handles raft internal network
+pub mod raftserver;
+pub mod shared; // connects the pieces // static configuration
